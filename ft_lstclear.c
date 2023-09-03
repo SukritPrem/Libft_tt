a417ee15323562c329379d097cf2298aa4aa0f49
Sukrit Pipitkul <spipitku@z1t3c2.42bangkok.com>
2023-09-03 21:00:41 +0700

    first commit

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spipitku <spipitku@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/31 13:31:59 by spipitku          #+#    #+#             */
+/*   Updated: 2023/09/03 20:28:49 by spipitku         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	tmp = *lst;
+	if (!lst || !del)
+		return ;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+}
+
+
